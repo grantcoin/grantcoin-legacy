bbc080c2595ba1d8051fd87a88f7a06600eb1f6d
Raskal8 <annulus1@hotmail.com>
2016-06-03 03:44:27 -0400

    Added "walletnotify" Command

--- src/main.cpp
+++ src/main.cpp
@@ -1587,14 +1587,6 @@ bool Reorganize(CTxDB& txdb, CBlockIndex* pindexNew)
 }
 
 
-static void
-runCommand(std::string strCommand)
-{
-    int nErr = ::system(strCommand.c_str());
-    if (nErr)
-        printf("runCommand error: system(%s) returned %d\n", strCommand.c_str(), nErr);
-}
-
 // Called from inside SetBestChain: attaches a block to the new best chain being built
 bool CBlock::SetBestChainInner(CTxDB& txdb, CBlockIndex *pindexNew)
 {
